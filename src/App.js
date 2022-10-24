0e92ccdc385a73a9f487e746ba43a11170b5e478
Roger <88466461+RogerRandomDev@users.noreply.github.com>
2022-10-24 15:20:46 -0700

    button

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './css/main.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="thickButton roundBorder">test</button>
+    </div>
+  );
+}
+
+export default App;
